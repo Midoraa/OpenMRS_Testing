a1acb8ccbf528181fe1c35664975a446393a598d
DE160490 <thanhncde160490@fpt.edu.vn>
2023-07-19 16:31:03 +0700

    "Test Code"

---
+++ src/test/java/model/Account.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Account {
+    private String username;
+    private String password;
+    private String location;
+
+    public Account() {
+    }
+
+    public Account(String username, String password, String location) {
+        this.username = username;
+        this.password = password;
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/test/Test1.java
@@ -0,0 +1,9 @@
+package test;
+
+import function.Login;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Login.TestLogin();
+    }
+}
